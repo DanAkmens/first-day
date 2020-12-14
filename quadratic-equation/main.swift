07d2afbdb70b3dc60274d96fcc7076e1383cc9a3
Dan <Bdylan998@gmail.com>
2020-12-14 10:27:06 +0200

    First Home work, all 3 solutions in one file

--- quadratic-equation/main.swift
+++ quadratic-equation/main.swift
@@ -7,5 +7,40 @@
 
 import Foundation
 
-print("Hello, World!")
+// quadratic equation
+func solveEquation(a: Double, b: Double, c: Double) -> (sum: Double, subtract: Double) {
+    
+    let newB = pow(b, 2)
+    return ((-b + sqrt(newB - (b * a * c))) / (2 * a), (-b - sqrt(newB - (b * a * c))) / (2 * a))
+}
 
+
+var newResult = solveEquation(a: 1, b: 4, c: -21)
+print(newResult.sum)
+print(newResult.subtract)
+
+// Right triangle
+func solveRightTriangle(a: Double, b: Double) -> (perimeter: Double, square: Double, hypotenuse: Double) {
+    
+    let c = sqrt(a * a + b * b)
+    return ((a + b + c), (a * b) / 2, sqrt(sqrt(a) + sqrt(b)))
+}
+
+let myHypotenuse = solveRightTriangle(a: 6, b: 8)
+// print(myHypotenuse.hypotenuse)
+
+// bank deposit
+func bankDepositCalculator(amountOfDeposit: Int, percentage: Int, yearsOfDeposit: Int) -> Int {
+    
+    var firstYear = (amountOfDeposit * percentage) / 100
+    print(firstYear)
+    for _ in 1..<yearsOfDeposit {
+        firstYear += (amountOfDeposit * percentage) / 100
+        print(firstYear)
+    }
+    return firstYear + amountOfDeposit
+}
+
+
+let myDeposit = bankDepositCalculator(amountOfDeposit: 100, percentage: 8, yearsOfDeposit: 3)
+print(myDeposit)
